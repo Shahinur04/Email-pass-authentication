5598a019ac83d548d011e89ace6f167865712ae7
HmSani6699 <shahinur04042002@gmail.com>
2023-04-17 00:07:29 +0600

    email pas

--- src/Components/Register/SignUp.jsx
+++ src/Components/Register/SignUp.jsx
@@ -1,18 +1,34 @@
-import React from 'react';
+import React, { useState } from "react";
 
 const SignUp = () => {
-    return (
-        <div>
-            <h2>If you new please register now</h2>
-            <form >
-                <input type="email" name="email" id="email" placeholder='your email' />
-                <br />
-                <input type="password" name="password" id="password" placeholder='your password' />
-                <br />
-                <input type="submit" value="Register" />
-            </form>
-        </div>
-    );
+  const [email, setEmail] = useState([]);
+  const handleChange = (event) => {
+    console.log(event.target.value);
+    setEmail(event.target.value);
+  };
+  return (
+    <div>
+      <h2>If you new please register now</h2>
+      <form>
+        <input
+          onChange={handleChange}
+          type="email"
+          name="email"
+          id="email"
+          placeholder="your email"
+        />
+        <br />
+        <input
+          type="password"
+          name="password"
+          id="password"
+          placeholder="your password"
+        />
+        <br />
+        <input type="submit" value="Register" />
+      </form>
+    </div>
+  );
 };
 
 export default SignUp;
